c4afb09f11dc9058f5864f036b99f357b809e725
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2022-01-28 01:07:00 +0800

    Feat: mobile wrapper component for desktop devices

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import { Box, Center } from '@chakra-ui/react';
+import { ReactNode } from 'react';
+
+const [MOBILE_VW, MOBILE_VH] = ['414px', '896px'] as const;
+
+const MobileWrapper = ({ children }: { children: ReactNode }) => {
+    return (
+        <Center h="100vh">
+            <Box
+                w={`min(100vw,${MOBILE_VW})`}
+                h={`min(100vh,${MOBILE_VH})`}
+                bg="gray.100"
+                position="absolute"
+            >
+                {children}
+            </Box>
+        </Center>
+    );
+};
+
 function App() {
-    return <div className="App"></div>;
+    return <MobileWrapper>test</MobileWrapper>;
 }
 
 export default App;
