ff31378e75ed400bfc8e76a49c375b06ecf871a6
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2022-01-28 00:05:10 +0800

    Chore: add chakra ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { ChakraProvider } from '@chakra-ui/react';
+
+const Providers = ({ children }: { children: ReactNode }) => (
+    <ChakraProvider> {children} </ChakraProvider>
+);
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Providers>
+            <App />
+        </Providers>
     </React.StrictMode>,
     document.getElementById('root')
 );
