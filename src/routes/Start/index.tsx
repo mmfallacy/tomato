a16da583c46ad0586d96b9d264b1b531bf09888c
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2022-01-31 20:47:38 +0800

    Fix(Start): change colors to fit new palette definitions

--- src/routes/Start/index.tsx
+++ src/routes/Start/index.tsx
@@ -24,10 +24,8 @@ const Action = (props: ButtonProps) => (
     <Button
         boxSize="4rem"
         p="0"
-        bg="accent.600"
         borderRadius="full"
-        _hover={{ transform: 'scale(1.1)', bg: 'accent.300' }}
-        _active={{ transform: 'scale(0.9)', bg: 'accent.700' }}
+        colorScheme="accent"
         {...props}
     >
         <AiOutlineArrowRight boxSize="50%" fill="white" />
@@ -45,13 +43,13 @@ const Start = () => {
             <Illustration1 pos="absolute" right="0" top="8rem" />
 
             <Flex direction="column" p="1.25rem" pos="absolute" bottom="4rem">
-                <Heading fontSize="5xl" color="accent.600">
+                <Heading fontSize="5xl" color="accent.500">
                     Tomato
                 </Heading>
 
                 <Filler h="2rem" />
 
-                <Heading fontSize="xl" maxW="70%" color="accent.600">
+                <Heading fontSize="xl" maxW="70%" color="accent.500">
                     A pomodoro app that gets you working
                 </Heading>
 
