d3b1c73a799e3302556b32b76daeaa6a2c1e66a7
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2022-01-31 17:46:28 +0800

    Feat(Login): add page constituents and layout

--- src/routes/Login/index.tsx
+++ src/routes/Login/index.tsx
@@ -1,5 +1,38 @@
+import { Box, Button, chakra, Circle, Flex, Heading } from '@chakra-ui/react';
+import { Filler } from '../../components';
+import { ReactComponent as Illustration2Raw } from './Illustration2.svg';
+
+const Illustration2 = chakra(Illustration2Raw);
+
 const Login = () => {
-    return <div>test</div>;
+    return (
+        <Box pos="relative" bg="brand.300" boxSize="100%">
+            <Heading>Tomato</Heading>
+            <Circle
+                bg="white"
+                size="3xl"
+                r
+                pos="absolute"
+                top="-2rem"
+                right="-1rem"
+            />
+            <Illustration2 pos="absolute" top="8rem" />
+
+            <Flex
+                pos="absolute"
+                direction="column"
+                p="1.25rem 3rem"
+                bottom="4rem"
+                w="100%"
+            >
+                <Button>Sign In</Button>
+                <Filler h="2rem" />
+                <Button colorScheme="blue" variant="outline">
+                    Use as Guest
+                </Button>
+            </Flex>
+        </Box>
+    );
 };
 
 export default Login;
