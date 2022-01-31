869ecdf67bbde0260a217bb24a5fa7474e4b445a
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2022-01-31 20:50:11 +0800

    Feat: add scale transforms for hover and active button states

--- src/utils/styles.tsx
+++ src/utils/styles.tsx
@@ -1,7 +1,21 @@
 import { extendTheme } from '@chakra-ui/react';
 import { Colors } from './colors';
 
+const ButtonOverrides = {
+    baseStyle: {
+        _hover: {
+            transform: 'scale(1.025)',
+        },
+        _active: {
+            transform: 'scale(0.975)',
+        },
+    },
+};
+
 export const Theme = extendTheme({
+    components: {
+        Button: ButtonOverrides,
+    },
     styles: {
         global: {
             'html, body, .root': {
