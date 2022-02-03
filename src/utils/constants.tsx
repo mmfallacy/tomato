3a11118f412ebca72a24dc0a9f2ef15c16638693
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2022-02-03 20:40:12 +0800

    Chore: add default chakra sizes as constant (from docs)

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,1 +1,27 @@
 export const [MOBILE_VW, MOBILE_VH] = ['414px', '896px'] as const;
+
+export const DEFAULT_SIZES_CHAKRA = {
+    max: 'max-content',
+    min: 'min-content',
+    full: '100%',
+    '3xs': '14rem',
+    '2xs': '16rem',
+    xs: '20rem',
+    sm: '24rem',
+    md: '28rem',
+    lg: '32rem',
+    xl: '36rem',
+    '2xl': '42rem',
+    '3xl': '48rem',
+    '4xl': '56rem',
+    '5xl': '64rem',
+    '6xl': '72rem',
+    '7xl': '80rem',
+    '8xl': '90rem',
+    container: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+    },
+};
